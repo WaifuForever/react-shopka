0631d0b0f7b889560e003c1b8a05771b81ddbe91
Jojo <sirjoandersonwf@gmail.com>
2022-04-15 16:19:52 -0300

    add: utility classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import Routes from './routes';
 
+import './styles/colours.scss';
+import './styles/fonts.scss';
+import './styles/spacing.scss';
+
 const App = () => {
     return (
-        <div className="app">
+        <div className="app font-3">
             <Routes />
         </div>
     );
